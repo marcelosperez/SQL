ed2c6af35660e98e036ec045474e04ce4b636ca8
Marcelo Perez <jhkk1408@gmail.com>
2025-08-25 15:38:40 -0300

    query 10 added to PostgreSQL

---
+++ PostgreSQL Queries/query_010.sql
@@ -0,0 +1,11 @@
+-- Instructions: The video store is running an end-of-year inventory review 
+-- and wants to create a special catalog. The goal is to identify all 
+-- films that were released during or before 2005. For this special 
+-- catalog, the following information needs to be extracted from the database:
+-- The title of each film, presented in all lowercase for a clean, consistent look.
+-- The original price to rent the film.
+-- A new column that shows the price of the film with a 25% discount.
+SELECT LOWER(title) AS lower_case_title, rental_rate,
+            (rental_rate * 0.75) AS sale_rate
+FROM film
+WHERE release_year <= 2007;
