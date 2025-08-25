3a1abd030ecdb10a3d8ce9950f4d6e4fa91a1d97
Marcelo Perez <jhkk1408@gmail.com>
2025-08-25 15:31:04 -0300

    query 9 added to PostgreSQL

---
+++ PostgreSQL Queries/query_009.sql
@@ -0,0 +1,8 @@
+-- Problem: Write a SQL query to display the film titles in two ways: 
+-- all in uppercase and all in lowercase. 
+-- This exercise uses these two functions UPPER(column_name) and LOWER(column_name)
+SELECT title,
+             UPPER(title) AS UPPERCASE_TITLE,
+            LOWER(title) AS LOWERCASE_TITLE
+FROM film
+ORDER BY title;
